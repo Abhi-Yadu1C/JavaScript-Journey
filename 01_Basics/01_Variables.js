d3dfd8216ab50421f1b5ba5dacfa0b694fbcf3b7
Abhishek-Yadav <yadavabhishek7303@gmail.com>
2025-03-12 17:53:46 +0530

    variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "Abhi@000gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "234312523"
+accountCity = "Uttar Pradesh"
+
+console.log(accountId)
+console.table([accountEmail, accountId, accountPassword, accountCity])
